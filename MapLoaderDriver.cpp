b885f009ecf7b01c9f6f89a81504bdb23bc00aea
Daniel Thibault-Shea <daniel.thibaultshea@gmail.com>
2019-10-21 20:37:23 -0400

    maploader exit condition added as caused OOB error

--- MapLoaderDriver.cpp
+++ MapLoaderDriver.cpp
@@ -3,7 +3,7 @@
 int main() {
 	vector<string> locations = vector<string>();
 	
-	locations.push_back("C:\\Users\\win\\Documents\\europass.map");
+	locations.push_back("C:\\Users\\Dan\\Dropbox\\Concordia\\7 - Fall 2019\\COMP 345 - Advance program design with C++\\RiskLocal\\Maps\\solar.map");
 	int display= 0;
 	int index =0;
 	int country = 0;
@@ -16,7 +16,10 @@ int main() {
 	while(index != -1){
 		cout << "please specify which map do you want to see 0 - " << locations.size()-1 << "\n type -1 to exit"<< endl;
 		std::cin >> index;
-	
+		if (index < 0) { // exit loop and terminate
+			std::cout << "Thank you for using the maploader! Have a lovely day :)" << std::endl;
+			break;
+		}
 				for (int i = 0; i < maps.at(index)->continents->size(); i++) {
 					for (int j = 0; j < maps.at(index)->continents->at(i)->countries->size(); j++) {
 						cout << " ID: "<<maps.at(index)->continents->at(i)->countries->at(j)->getID() << " Name: " << maps.at(index)->continents->at(i)->countries->at(j)->getName() << " Belongs to continent " << maps.at(index)->continents->at(i)->getName() << endl;
