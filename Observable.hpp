105b94bd2c154e3dc72eaea18caab4983bd48a66
Daniel Thibault-Shea <daniel.thibaultshea@gmail.com>
2019-11-18 21:52:10 -0500

    Dany's changes to read alternate maps

---
+++ Observable.hpp
@@ -0,0 +1,27 @@
+//
+//  Observable.hpp
+//  Assignment3
+//
+//  Created by mushfiqur anik on 2019-11-17.
+//  Copyright © 2019 mushfiqur anik. All rights reserved.
+//
+
+#pragma once
+#include <list>
+using namespace std;
+
+class Observer;
+
+class Observable
+{
+public:
+    virtual ~Observable() {};
+    virtual void Attach(Observer *) = 0;
+    virtual void Detach(Observer *) = 0;
+    virtual void Notify() = 0;
+    virtual void Notify(int playerID, string phase, string action); // This is for the PhaseObserver View 
+protected:
+    Observable() {};
+private:
+    list<Observer*> _observers;
+};
